5e291178ec45179b85cfd184fcd9a92a0ed0bb24
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-09 21:35:57 -0200

    Add parent field and tree level

--- opps/comments/models.py
+++ opps/comments/models.py
@@ -1,14 +1,24 @@
 # coding: utf-8
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from mptt.models import MPTTModel, TreeForeignKey
+from mptt.managers import TreeManager
+
 from opps.core.models import NotUserPublishable, OwnedNotRequired
 
 
-class Comment(OwnedNotRequired, NotUserPublishable):
+class Comment(MPTTModel, OwnedNotRequired, NotUserPublishable):
     author_name = models.CharField(max_length=255)
     author_email = models.EmailField()
     path = models.CharField(max_length=255)
     body = models.TextField()
+    parent = TreeForeignKey('self', related_name='replay',
+                            null=True, blank=True,
+                            verbose_name=_(u'Parent'))
+
+    objects = TreeManager()
 
     def __unicode__(self):
         return u"{s.author_name} - {s.author_email} - {s.path}".format(
