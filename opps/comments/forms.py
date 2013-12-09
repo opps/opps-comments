a78ef065ff9fa2a84c7bea86e6a9d0c69455791d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-09 21:36:56 -0200

    Set field parent HiddenInput on form

--- opps/comments/forms.py
+++ opps/comments/forms.py
@@ -1,12 +1,14 @@
 #coding: utf-8
 
 from django import forms
+
 from .models import Comment
 
 
 class CommentsForm(forms.ModelForm):
     path = forms.CharField(widget=forms.HiddenInput)
+    parent = forms.CharField(widget=forms.HiddenInput())
 
     class Meta:
         model = Comment
-        fields = ["author_name", "author_email", "path", "body"]
+        fields = ["author_name", "author_email", "path", "body", "parent"]
