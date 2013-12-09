2d14da82c69b0d286034526987964f9d44053b04
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-09 21:37:30 -0200

    Return tree object on api

--- opps/comments/api.py
+++ opps/comments/api.py
@@ -7,3 +7,26 @@ from .models import Comment
 
 class CommentHandler(BaseHandler):
     model = Comment
+
+    def read(self, request):
+        def _node(node):
+            n = []
+            for i in node.get_children():
+                if i.published:
+                    obj = i.__dict__
+                    obj['comments'] = _node(i)
+                    n.append(obj)
+            return n
+
+        root = self.model.objects.filter(
+            level=0,
+            published=True).filter(**request.GET.dict())
+
+        l = []
+        for t in root:
+            n = {}
+            n = t.__dict__
+            n['comments'] = _node(t)
+            l.append(n)
+
+        return l
