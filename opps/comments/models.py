4da6b79db973101d2ba911be78a4e773a57b052a
Ellison Leao <ellisonleao@gmail.com>
2014-01-07 11:11:52 -0200

    Add translations strings on comment model

--- opps/comments/models.py
+++ opps/comments/models.py
@@ -10,10 +10,10 @@ from opps.core.models import NotUserPublishable, OwnedNotRequired
 
 
 class Comment(MPTTModel, OwnedNotRequired, NotUserPublishable):
-    author_name = models.CharField(max_length=255)
-    author_email = models.EmailField()
-    path = models.CharField(max_length=255)
-    body = models.TextField()
+    author_name = models.CharField(_(u'Author name'), max_length=255)
+    author_email = models.EmailField(_(u'Author email'))
+    path = models.CharField(_(u'Path'), max_length=255)
+    body = models.TextField(_('Body'))
     parent = TreeForeignKey('self', related_name='replay',
                             null=True, blank=True,
                             verbose_name=_(u'Parent'))
@@ -24,3 +24,7 @@ class Comment(MPTTModel, OwnedNotRequired, NotUserPublishable):
         return u"{s.author_name} - {s.author_email} - {s.path}".format(
             s=self
         )
+
+    class Meta:
+        verbose_name = _(u'Comment')
+        verbose_name_plural = _(u'Comments')
