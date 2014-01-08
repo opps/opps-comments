ee4935e11e15ed1a199114dbbc8f69c536c77d06
Ellison Leao <ellisonleao@gmail.com>
2014-01-08 18:41:24 -0200

    Add custom admin on Comment admin

--- opps/comments/admin.py
+++ opps/comments/admin.py
@@ -1,6 +1,28 @@
 # coding: utf-8
-
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from opps.core.admin import PublishableAdmin, apply_opps_rules
+
 from .models import Comment
 
-admin.site.register(Comment)
+@apply_opps_rules('comments')
+class CommentAdmin(PublishableAdmin):
+    list_display = ('author_name', 'author_email', 'body', 'published')
+    list_filter = ('published',)
+    search_fields = ('author_email',)
+
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('author_name', 'author_email', 'body', 'path')}),
+        (_(u'Relationships'), {
+            'fields': ('site', 'mirror_site', 'parent')}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available')}),
+    )
+
+    class Meta:
+        model = Comment
+
+admin.site.register(Comment, CommentAdmin)
